3b5da00783f7b212876733c0886dcc6c6a837fd5
Tam Pham <tampham47@live.com>
2019-07-16 11:20:30 +0700

    add styled-components, normalize.css

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: calc(100% - 2em);
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 75em;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <h1>Base frontend</h1>
+      <Container>
+        <h1>NASA Collection</h1>
+      </Container>
     )
   }
 };
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { hot } from 'react-hot-loader/root';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/app';
+import App from './components/App';
+
 const HotApp = hot(App);
 
 ReactDOM.render(
