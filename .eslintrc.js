699c2c58679ed1ff47cfeb1c4902ebd38e3a5011
Tam Pham <tampham47@live.com>
2019-07-16 12:22:41 +0700

    add eslint & commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,71 @@
+// ESLint configuration
+// http://eslint.org/docs/user-guide/configuring
+module.exports = {
+  parser: 'babel-eslint',
+
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+
+  globals: {
+    __DEV__: true,
+    FB: true,
+  },
+
+  env: {
+    browser: true,
+  },
+
+  rules: {
+    // Recommend not to leave any console.log in your code
+    // Use console.error, console.warn and console.info instead
+    // https://eslint.org/docs/rules/no-console
+    'no-console': [
+      'error',
+      {
+        allow: ['warn', 'error', 'info'],
+      },
+    ],
+
+    // Prefer destructuring from arrays and objects
+    // http://eslint.org/docs/rules/prefer-destructuring
+    'prefer-destructuring': [
+      'error',
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
+      },
+      {
+        enforceForRenamedProperties: false,
+      },
+    ],
+
+    // Allow .js files to use JSX syntax
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+
+    // Functional and class components are equivalent from React’s point of view
+    // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prefer-stateless-function.md
+    'react/prefer-stateless-function': 'off',
+
+    'no-underscore-dangle': 'off',
+    'arrow-body-style': 'off',
+  },
+
+  settings: {
+    // Allow absolute paths in imports, e.g. import Button from 'components/Button'
+    // https://github.com/benmosher/eslint-plugin-import/tree/master/resolvers
+    'import/resolver': {
+      node: {
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+};
